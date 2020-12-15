2733e18be174901a1deb2ffe2fb9b5a0301acebd
arne-vanmarcke <R0783508@student.vives.be>
2020-12-15 11:13:12 +0000

    Javascript page that is linked to html page signalR

---
+++ wwwroot/js/chat.js
@@ -0,0 +1,65 @@
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+//Disable send button until connection is established
+//document.getElementById("sendButton").disabled = true;
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " " + msg;
+    console.log(encodedMsg);
+    if(user.includes("_chbx"))
+    {
+        if(msg=='High')
+            document.getElementById(user).checked=true;
+        else
+            document.getElementById(user).checked=false;
+    }
+    else
+    {
+        var id=user.replace(':','').trim().replace(' ','');
+        document.getElementById(id).innerHTML=encodedMsg;
+    }
+    /*var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);*/
+});
+connection.start().then(function () {
+    //document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+document.getElementById("GPIO18_chbx").addEventListener("change", function (event) {
+    var user = document.getElementById("GPIO18_chbx").id;
+    var message = document.getElementById("GPIO18_chbx").checked==true ? 'High':'Low';
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    user=user.replace('GPIO','');
+    connection.invoke("setPin",parseInt(user.replace('_chbx','').trim()),(document.getElementById("GPIO18_chbx").checked)).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+document.getElementById("GPIO23_chbx").addEventListener("change", function (event) {
+    var user = document.getElementById("GPIO23_chbx").id;
+    var message = document.getElementById("GPIO23_chbx").checked==true ? 'High':'Low';
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    user=user.replace('GPIO','');
+    connection.invoke("setPin",parseInt(user.replace('_chbx','').trim()),(document.getElementById("GPIO23_chbx").checked)).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+document.getElementById("GPIO24_chbx").addEventListener("change", function (event) {
+    var user = document.getElementById("GPIO24_chbx").id;
+    var message = document.getElementById("GPIO24_chbx").checked==true ? 'High':'Low';
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    user=user.replace('GPIO','');
+    connection.invoke("setPin",parseInt(user.replace('_chbx','').trim()),(document.getElementById("GPIO24_chbx").checked)).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
